147f907709c25b51125f28052317396d67b0981b
Jonathan Duncan <jonathan.duncan@wallawalla.edu>
2019-09-26 09:20:09 -0700

    adding day 3 code

---
+++ in-class/0926/firstExample.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+ *
+ * In-Class Demonstration:  Prompt for two numbers and display their sum
+ *
+ * File Name: firstExample.cpp
+ * Course:    CPTR 141
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {  
+}
---
+++ in-class/0926/fixme.cpp
@@ -0,0 +1,40 @@
+/*************************************************************************
+ *
+ * In-Class Exercise:  Find and fix the errors in the following C++
+ *                     program so that it will compile and run 
+ *                     as expected.
+ *
+ * File Name: fixme.cpp
+ * Course:    CPTR 141
+ *
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   
+// assign some variable values
+    int a = 25;
+    b = 5;
+
+           // output several computations -- endl will end the line
+    cout<< " a + b = " << a+b<< endl
+   cout << " a * b = " << a * b <<endl;
+     cout   << " a / b = " <<    a / b << endl;
+
+    // assign a few more variables
+int grad_year;
+string college = Walla Walla University;
+
+    // ask for and save graduation year
+    cout << endl << "In what year will you graduate?;
+    cin >> grad_Year;
+
+    // output a sentence based on this input
+    cout << "You will graduate from " << college << " in ";
+    cout << grad_year << "." endl;
+
+         return    0;
+}
