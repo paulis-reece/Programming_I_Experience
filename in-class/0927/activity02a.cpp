db8920f489fa13ec004f583d050f573931fa30bf
Jonathan Duncan <jonathan.duncan@wallawalla.edu>
2019-09-27 10:34:03 -0700

    adding code for pogil #2

---
+++ in-class/0927/activity02a.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << 16 + 3 << endl;   
+  cout << 16 - 3 << endl;
+  cout << 16 * 3 << endl;   
+  cout << 16 / 3 << endl;   
+  cout << 16 % 3 << endl;   
+}
