42ca515d8bf76ce6d543f187c27a394281ef811e
Jonathan Duncan <jonathan.duncan@wallawalla.edu>
2019-10-02 12:26:05 -0700

    releasing homework #2

---
+++ homework/hw02/makeChange.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+ *
+ * hw02: A change computation program
+ *
+ * File Name: makeChange.cpp
+ * Name:      ?
+ * Course:    CPTR 141
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // define variables
+
+  // Prompt for and collect input
+
+  // convert to cents and compute change
+
+  // now display number of dollars and coins
+
+  return 0;
+}
