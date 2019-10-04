9768f6a1ff8f998685bdd7eb27e2daf92fa8def6
Reece Reklai <reece.reklai@wallawalla.edu>
2019-10-04 01:50:54 +0000

    autosave

--- homework/hw00/quote.cpp
+++ homework/hw00/quote.cpp
@@ -17,7 +17,7 @@ int main() {
 
     cout << "It should be mandatory" << endl << "  that you" << endl << "    understand computer science." << endl << endl;
 
-    cout <<  "-   "   << "will.i.am" << endl;
+    cout <<  "- "   << "will.i.am" << endl;
 
     return 0;
 }
