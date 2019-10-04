fe6ceebff944b2d70974286ead2eac8bf56e67a8
Jonathan Duncan <jonathan.duncan@wallawalla.edu>
2019-10-04 11:01:19 -0700

    releasing inclass code

---
+++ in-class/1004/activity04a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // variable for testing
+  unsigned short int test;
+
+  // assign the largest value possible
+
+  // then add one
+
+  // then output the result
+  cout << "The value is: " << test << endl;
+
+  return 0;
+}
---
+++ in-class/1004/activity04c.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // test your code snippet here
+
+  return 0;
+}
