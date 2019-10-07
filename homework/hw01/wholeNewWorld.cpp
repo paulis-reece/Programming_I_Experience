f96ab73de492a67a9cf2c383feb89029078d4baf
Reece Reklai <reece.reklai@wallawalla.edu>
2019-10-07 05:31:57 +0000

    autosave

--- homework/hw01/wholeNewWorld.cpp
+++ homework/hw01/wholeNewWorld.cpp
@@ -3,21 +3,52 @@
  * hw01: A mad-libs introduction to a famous song from Aladdin
  *
  * File Name: wholeNewWorld.cpp
- * Name:      ?
+ * Name:      Reece Reklai
  * Course:    CPTR 141
  *
  */
 
-#include <iostream>  // included for cin and cout processing
+#include <iostream> // included for cin and cout processing
 using namespace std;
-
+#include <string>
 int main() {
-   
-    // define your variables below
 
-    // gather user input using cin << variableName
+  // define your variables below
+  string ADJECTIVE1;
+  string NOUN;
+  string ADJECTIVE2;
+  string COMMAND;
+  string QUESTION;
+  string VERB;
+  string VERBING;
+  string ADJECTIVE3;
+  string PREPOSITION;
 
-    // print out the "song"
+  // gather user input using cin << variableName
+  cin >> ADJECTIVE1;
+  cin >> NOUN;
+  cin >> ADJECTIVE2;
+  cin >> COMMAND;
+  cin >> QUESTION;
+  cin >> VERB;
+  cin >> VERBING;
+  cin >> ADJECTIVE3;
+  cin >> PREPOSITION;
 
-    return 0;
+  // print out the "song"
+
+  cout << "A whole " << ADJECTIVE1 << " " << NOUN << endl;
+  cout << "A new " << ADJECTIVE2 << " point of view" << endl;
+  cout << "No one to tell us " << COMMAND << endl;
+  cout << "Or " << QUESTION << " to " << VERB << endl;
+  cout << "Or say we're only " << VERBING << endl;
+  cout << endl;
+  cout << "A whole " << ADJECTIVE1 << " " << NOUN << endl;
+  cout << "A " << ADJECTIVE3 << " place I never knew" << endl;
+  cout << "But now from way " << PREPOSITION << " here" << endl;
+  cout << "It's crystal clear" << endl;
+  cout << "that now I'm in a whole " << ADJECTIVE1 << " " << NOUN
+       << " with you." << endl;
+
+  return 0;
 }
