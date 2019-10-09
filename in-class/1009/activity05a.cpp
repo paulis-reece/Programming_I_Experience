a789ee2990118a43797b753b1ad6900eb556a5d5
Jonathan Duncan <jonathan.duncan@wallawalla.edu>
2019-10-09 11:11:11 -0700

    releasing activities

---
+++ in-class/1009/activity05a.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // variable for testing
+  int x = 4, y = 5, z = 4;
+
+  // put your condition inside the "if( )" statement
+  if (x > y) {
+
+    // this prints if the condition inside "if( )" is true
+    cout << "true" << endl;
+
+  } else {
+
+    // this prints if it is false
+    cout << "false" << endl;
+  }
+
+  return 0;
+}
