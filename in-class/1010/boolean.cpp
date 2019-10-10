7d1a419351455c1bdaaf622fdee8c70e0bf910f8
Jonathan Duncan <jonathan.duncan@wallawalla.edu>
2019-10-09 17:01:46 -0700

    releasing classwork

---
+++ in-class/1010/boolean.cpp
@@ -0,0 +1,28 @@
+/*************************************************************************
+ *
+ * In-Class Exercise:  This program gives us the results of various
+ *                     combinatons of boolean and relational operators.
+ *
+ * File Name: boolean.cpp
+ * Username:  ?
+ * Username:  ?
+ * Course:    CPTR 141
+ * Date:      @@date@@
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    // part 1 -- find the values of the given Boolean expressions
+
+   
+    // part 2 -- find the values of the given Boolean expressions with
+    // relational operators
+    int limit = 10;
+    int count = 0;
+
+    return 0;
+}
