3d3310851e2cae503860a9a68352ce9013347f96
Jonathan Duncan <jonathan.duncan@wallawalla.edu>
2019-10-09 22:41:20 -0700

    releasing homework #4

---
+++ homework/hw04/numberPicker.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+ *
+ * hw04: A program to prompt the user for five numbers and then randomly
+ *       pick one of them.
+ *
+ * File Name: numberPicker.cpp
+ * Name:      ?
+ * Course:    CPTR 141
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  return 0;
+   
+}
