c8ffed293d1ec251283240aded3ad4c32f49d744
Reece Reklai <reece.reklai@wallawalla.edu>
2019-10-15 23:49:29 +0000

    autosave

--- homework/hw04/numberPicker.cpp
+++ homework/hw04/numberPicker.cpp
@@ -20,28 +20,39 @@ int main() {
   int number3;
   int number4;
   int number5;
+  int Random;
 
   cout << "Enter Random Seed: ";
   cin >> Seed;
 
   cout << endl << "Enter First Number: ";
   cin >> number1;
-  srand(number1);
 
   cout << "Enter Second Number: ";
   cin >> number2;
-  srand(number2);
 
   cout << "Enter Third Number: ";
   cin >> number3;
-  srand(number3);
+
   cout << "Enter Fourth Number: ";
   cin >> number4;
-  srand(number4);
 
   cout << "Enter Fifth Number: ";
   cin >> number5;
-  srand(number5);
+
+  srand(Seed);
+  Random = rand() % 5 + 1;
+  if (Random == 1) {
+    cout << "I choose: " << number1 << endl;
+  } else if (Random == 2) {
+    cout << "I choose: " << number2 << endl;
+  } else if (Random == 3) {
+    cout << "I choose: " << number3 << endl;
+  } else if (Random == 4) {
+    cout << "I choose: " << number4 << endl;
+  } else if (Random == 5) {
+    cout << "I choose: " << number5 << endl;
+  }
 
   return 0;
 }
