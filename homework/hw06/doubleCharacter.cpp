911737fa512aa8a3ff1cc79a67c642120564cb5e
Jonathan Duncan <jonathan.duncan@wallawalla.edu>
2019-10-14 18:04:09 -0700

    releasing hw06

---
+++ homework/hw06/doubleCharacter.cpp
@@ -0,0 +1,21 @@
+/****************************************************************************
+ *
+ * hw06: Prompt the user for a word and then check it for double characters.
+ *
+ * File Name: double.cpp
+ * Name:      ?
+ * Course:    CPTR 141
+ *
+ */
+
+#include <iostream> // for cout and cin
+#include <string>   // for string commands
+using namespace std;
+
+int main() {
+
+  /* YOUR CODE GOES HERE */
+   
+  return 0;
+   
+}
