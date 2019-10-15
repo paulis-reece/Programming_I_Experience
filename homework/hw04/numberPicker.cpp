94a77ed8a2dc52d04042ed3550d76edeace45511
Reece Reklai <reece.reklai@wallawalla.edu>
2019-10-15 04:49:03 +0000

    autosave

--- homework/hw04/numberPicker.cpp
+++ homework/hw04/numberPicker.cpp
@@ -4,16 +4,53 @@
  *       pick one of them.
  *
  * File Name: numberPicker.cpp
- * Name:      ?
+ * Name:      Reece Reklai
  * Course:    CPTR 141
  *
  */
 
+#include <cstdlib>
 #include <iostream>
 using namespace std;
 
 int main() {
+    int Seed;
+    int number1;
+    int number2;
+    int number3;
+    int number4;
+    int number5;
+
+
+    cout << "Enter Random Seed: ";
+    cin >> Seed;
+    srand(Seed);
+
+    cout << endl << "Enter First Number: ";
+    cin >> number1;
+    srand(number1);
+
+    cout << "Enter Second Number: ";
+    cin >> number2;
+srand(number2);
+
+    cout << "Enter Third Number: ";
+    cin >> number3;
+srand(number3);
+    cout << "Enter Fourth Number: ";
+    cin >> number4;
+    srand(number4);
+
+    cout << "Enter Fifth Number: ";
+    cin >> number5;
+srand(number5);
+
+
+
+
+
+
 
   return 0;
-   
+
 }
