1aee33ec0cbc4a5ece3db7a9a039a366cfb69b80
Jonathan Duncan <jonathan.duncan@wallawalla.edu>
2019-10-18 10:58:59 -0700

    releasing in-class files

---
+++ in-class/1018/activity08a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string name;
+  cout << "Enter your name: ";
+  cin >> name;
+  for (int x = 0; x < 10; x += 1) {
+    cout << name << endl;
+  }
+  cout << "Nice to meet you!" << endl;
+}
---
+++ in-class/1018/activity08c.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // define our variable
+  int x;
+
+  // row one
+  x = 5;
+  cout << "Expression: Value " << (3 + 2 * (x++)) << endl;
+  cout << "x Value: " << x << endl << endl;
+
+  // row two
+  x = 3;
+  cout << "Expression: Value " << (5 - (++x) / 2) << endl;
+  cout << "x Value: " << x << endl << endl;
+
+  // row three
+  x = 1;
+  cout << "Expression: Value " << (6 * (--x)) << endl;
+  cout << "x Value: " << x << endl << endl;
+
+  // row four
+  x = -3;
+  cout << "Expression: Value " << (2 + (x--)) << endl;
+  cout << "x Value: " << x << endl << endl;
+}
