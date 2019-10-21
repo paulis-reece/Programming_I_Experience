b9cec9ed8bf1008f712aae443f933ffbdcbafb25
Reece Reklai <reece.reklai@wallawalla.edu>
2019-10-21 03:49:31 +0000

    autosave

--- homework/hw05/decisionTree.cpp
+++ homework/hw05/decisionTree.cpp
@@ -8,24 +8,17 @@
  *
  */
 
-#include <iostream>
-#include <iomanip>
 #include <ctime>
+#include <iomanip>
+#include <iostream>
 using namespace std;
 
 int main() {
-   int choice;
-    int time;
-    int numBooksCarry;
-    string bus;
-    string walk;
-    int minutes;
-    int books;
-
-
-
-
-
+  int choice;
+  int time;
+  int numBooksCarry;
+  int minutes;
+  int books;
 
   /* YOUR CODE GOES HERE */
   cout << "a. What is the weather like?" << endl;
@@ -34,28 +27,43 @@ int main() {
   cout << "   3 = Rainy" << endl;
   cout << "Enter choice: ";
   cin >> choice;
-  switch(choice){
-      case 3:
+  switch (choice) {
+  case 3:
+    cout << "You had better ride the bus to school.";
+    break;
+  case 1:
+    cout << "b. How much time do you have, in minutes? ";
+    cin >> minutes;
+    if (minutes < 30) {
+      cout << endl;
       cout << "You had better ride the bus to school.";
+    } else if (minutes >= 30) {
+      cout << "c. How many books do you have? ";
+      cin >> books;
+      if (books <= 2) {
+        cout << "You should walk to school.";
+      } else if (books > 2)
+        cout << "You had better ride the bus to school.";
       break;
-      case 1:
-      cout << "b. How much time do you have, in minutes? ";
-      cin >> minutes;
-      if(minutes < 30) {
-          cout << endl;
-          cout << "You had better ride the bus to school.";
-          } else if(minutes >= 30){
-              cout << "c. How many books do you have? ";
-              cin >> books;
-           if(books <= 2) {
-              cout << "You should walk to school.";
+    }
 
-              break;}}
-
-
+  case 2:
+    cout << "b. How much time do you have, in minutes? ";
+    cin >> minutes;
+    if (minutes < 20) {
+      cout << endl;
+      cout << "You had better ride the bus to school.";
+    } else if (minutes >= 20) {
+      cout << "c. How many books do you have? ";
+      cin >> books;
+      if (books <= 4) {
+        cout << "You should walk to school.";
+      } else if (books > 4) {
+        cout << "You had better ride the bus to school.";
+        break;
+      }
+    }
   }
-
-
 
   return 0;
 }
