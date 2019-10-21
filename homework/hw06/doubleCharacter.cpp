8091eea5384d3e95eea4e58d6deb7c6a9bb02a13
Reece Reklai <reece.reklai@wallawalla.edu>
2019-10-21 22:50:20 +0000

    autosave

--- homework/hw06/doubleCharacter.cpp
+++ homework/hw06/doubleCharacter.cpp
@@ -3,7 +3,7 @@
  * hw06: Prompt the user for a word and then check it for double characters.
  *
  * File Name: double.cpp
- * Name:      ?
+ * Name:      Reece Reklai
  * Course:    CPTR 141
  *
  */
@@ -13,9 +13,16 @@
 using namespace std;
 
 int main() {
+    int word;
+    string wordLetter;
 
+
+cout << "Welcome to the DoubleChecker(TM) word checker" << endl;
+cout << "=============================================" << endl;
+cout << "Enter a word to check: ";
+cin >> word;
   /* YOUR CODE GOES HERE */
-   
+
   return 0;
-   
+
 }
