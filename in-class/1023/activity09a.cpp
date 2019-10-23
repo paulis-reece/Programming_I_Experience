e04208f71e3e30e4c0a3b93e1336a25ad14caf4b
Jonathan Duncan <jonathan.duncan@wallawalla.edu>
2019-10-23 10:40:07 -0700

    releasing in-class work

---
+++ in-class/1023/activity09a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+  string name;
+  cout << "What is your name? ";
+  cin >> name;
+
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 3; j++) {
+      cout << name << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
---
+++ in-class/1023/activity09c.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int numRows = 5;
+  for (int j = numRows; j > 0; j--) {
+    for (int i = 1; i <= j; i++) {
+      cout << i << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
