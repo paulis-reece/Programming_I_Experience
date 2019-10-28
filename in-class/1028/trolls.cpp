1d811e609e046c549e29d4a1584a82067dd813e5
Jonathan Duncan <jonathan.duncan@wallawalla.edu>
2019-10-28 10:06:15 -0700

    releasing in-class assignment

---
+++ in-class/1028/trolls.cpp
@@ -0,0 +1,31 @@
+/*************************************************************************
+ *
+ * In-Class Exercise: Using Vectors to Store Prices and More
+ *
+ * File Name: trolls.cpp
+ * Course:    CPTR 141
+ */
+
+#include <iostream> // for cin and cout
+#include <vector>   // for vectors
+using namespace std;
+
+/*====================================================================
+ * Main program
+ */
+
+int main() {
+
+  // define your variables here
+  const int NUM = 5;
+
+  // loop to collect information
+  for (int i = 0; i < NUM; i++) {
+  }
+
+  // loop to print out prices
+  for (int i = 1; i < NUM; i++) {
+  }
+
+  return 0;
+}
