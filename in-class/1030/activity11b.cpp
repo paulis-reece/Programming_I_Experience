76f0e389416710ae5d8571f0ef43449dcc1981f1
Jonathan Duncan <jonathan.duncan@wallawalla.edu>
2019-10-30 10:33:01 -0700

    releasing pogil files

---
+++ in-class/1030/activity11b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+void printArea(double radius){
+   double area = 3.14159 * pow(radius,2);
+   cout << fixed << setprecision(2);
+   cout << "The area of a circle with radius "
+        << radius << " is " << area << endl;
+}
+
+int main() {
+   printArea(3);
+   printArea(4.5);
+}
