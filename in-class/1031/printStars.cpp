e212880e21f67b2368c5a955e62f532fffedfb1d
Reece Reklai <reece.reklai@wallawalla.edu>
2019-10-31 20:51:12 +0000

    autosave

--- in-class/1031/printStars.cpp
+++ in-class/1031/printStars.cpp
@@ -6,24 +6,30 @@
  * Course:    CPTR 141
  */
 
-#include <iostream>  // for cin and cout
+#include <iostream> // for cin and cout
 using namespace std;
 
 // original function
-void printStars(int num) {
-   for (int i=0; i<num; i++) {
-      cout << '*';
-   }                   
+void printStars(int num, char symbol, bool newLine) {
+  for (int i = 0; i < num; i++) {
+    cout << symbol;
+  }
+  if(newLine){
+      cout <<endl;
+  }
 }
-
 
 // Here is the main program
 int main() {
-   
-    // Test your functions in the main program
-    printStars(2+3);
-   
-    return 0;
+
+  // Test your functions in the main program
+  printStars(2 + 3, '*', true);
+
+  printStars(2 + 3, '=', false);
+
+  printStars(2 + 3, '-', true);
+
+  return 0;
 }
 
 // Function definitions go here
