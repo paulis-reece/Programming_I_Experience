bb5e5eaaa59e451fec53354118f79fd52f97e930
Reece Reklai <reece.reklai@wallawalla.edu>
2019-11-04 10:54:35 +0000

    autosave

--- homework/hw08/gradeCalculator.cpp
+++ homework/hw08/gradeCalculator.cpp
@@ -3,25 +3,71 @@
  * hw08: Calculate the average for a user-entered number of grades
  *
  * File Name:  gradeCalculator.cpp
- * Name:       ?
+ * Name:       Reece Reklai
  * Course:     CPTR 141
  *
  */
 
 #include <iomanip>  // for format output
 #include <iostream> // for cout and cin
-#include <vector>   // for vectors
+#include <numeric>
+#include <vector> // for vectors
 using namespace std;
 
 int main() {
+  double gradeNum;
+  int i = 1;
+  double ileration = 1;
+  bool average;
 
   // opening prompt
   cout << "Enter a score between 0 and 100 on each line." << endl;
   cout << "Enter a negative number to finish." << endl;
   cout << endl;
+  cout << "Grade #1: ";
+  cin >> gradeNum;
+  vector<double> gradeBook = {gradeNum};
 
   /* Your Code Here */
-   
+  if (gradeNum < 0) {
+    average = false;
+  }
+  if (gradeNum >= 0) {
+    average = true;
+    while (gradeNum >= 0) {
+      if (gradeNum > 100) {
+        while (gradeNum > 100) {
+          i = i;
+          cout << "Error!  Grades may not be more than 100." << endl;
+          cout << "Grade #" << i << ": ";
+          cin >> gradeNum;
+          if (gradeNum >= 0 && gradeNum <= 100) {
+            gradeBook.push_back(gradeNum);
+            ileration++;
+          } else if (gradeNum < 0) {
+            break;
+          }
+        }
+      }
+      if (gradeNum >= 0 && gradeNum <= 100) {
+        i++;
+        cout << "Grade #" << i << ": ";
+        cin >> gradeNum;
+        if (gradeNum >= 0 && gradeNum <= 100) {
+          gradeBook.push_back(gradeNum);
+          ileration++;
+        }
+      }
+    }
+  }
+
+  if (average == false) {
+    cout << "No grades were entered.";
+
+  } else if (average == true) {
+    cout << "Your avearge is: ";
+    cout << fixed << setprecision(2)
+         << accumulate(gradeBook.begin(), gradeBook.end(), 0) / ileration;
+  }
   return 0;
-   
 }
---
+++ projects/proj02/MyProject2
@@ -0,0 +1,20 @@
+/*************************************************************************
+ *
+ * In-Class Exercise:  A "hello world" program.
+ *
+ * File Name: hello.cpp
+ * Name:      ?
+ * Course:    CPTR 141
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    // print out some text (replace the question mark with your name)
+    cout << "Hello World! My name is ? and this may be my first program in CPTR 141!\n";
+
+    return 0;
+}
