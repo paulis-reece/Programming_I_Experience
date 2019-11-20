1c5e200df87d4f28e49133bfee0f08217bbc8ae2
Jonathan Duncan <jonathan.duncan@wallawalla.edu>
2019-11-20 10:56:39 -0800

    releasing in-class activities

---
+++ in-class/1120/activity17c.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int number;
+  string name;
+  cout << "Enter Number: ";
+  cin >> number;
+  cout << "Enter Name: ";
+  cin >> name;
+  for (int i = 0; i < number; i++) {
+    cout << name << endl;
+  }
+}
