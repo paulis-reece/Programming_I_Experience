116b9a4870c804126c599f41646f92335d112f19
Jonathan Duncan <jonathan.duncan@wallawalla.edu>
2019-11-20 22:07:33 -0800

    releasing homework 14

---
+++ homework/hw14/perimeterFunctions.h
@@ -0,0 +1,14 @@
+/*************************************************************************
+ *
+ * hw14: Find the perimeter of several different types of figures
+ *
+ * File Name: perimeter.h
+ * Name:      ???
+ * Course:    CPTR 141
+ *
+ */
+
+extern const double PI;
+
+double findPerimeter(double length, int sides);
+double findPerimeter(double radius);
