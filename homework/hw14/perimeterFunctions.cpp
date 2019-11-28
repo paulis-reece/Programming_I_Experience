523bedb8100ac69099ba588e1ac265c8f39cff2a
Reece Reklai <reece.reklai@wallawalla.edu>
2019-11-28 06:35:59 +0000

    autosave

--- homework/hw14/perimeterFunctions.cpp
+++ homework/hw14/perimeterFunctions.cpp
@@ -3,11 +3,22 @@
  * hw14: Find the perimeter of several different types of figures
  *
  * File Name: perimeterFunctions.cpp
- * Name:      ???
+ * Name:      Reece Reklai
  * Course:    CPTR 141
  *
  */
 
 #include "perimeterFunctions.h"
-
+#include <vector>
 const double PI = 3.141592653689;
+
+double findPerimeter(double length, int sides){
+double result;
+result = length * sides;
+return result;
+}
+double findPerimeter(double radius){
+double circumference;
+circumference = 2 * PI * radius;
+return circumference;
+}
