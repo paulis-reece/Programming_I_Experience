c67e9e8655c0c9a187b02dc5b682d0587933c59f
Reece Reklai <reece.reklai@wallawalla.edu>
2019-11-28 07:42:06 +0000

    autosave

--- homework/hw15/arrayManipulate.cpp
+++ homework/hw15/arrayManipulate.cpp
@@ -36,8 +36,10 @@ void accumulate(int array[], int arrayNum) {
   }
 }
 void reverse(int array[], int arrayNum) {
+int counter = 0;
   assert(arrayNum > 0);
-  for (int i = 0; i < arrayNum; ++i) {
-    array[i] = array[arrayNum - 1 - i];
+  for (int i = arrayNum - 1; 0 <= i;  i--) {
+    array[i] = array[counter];
+    counter ++;
   }
 }
