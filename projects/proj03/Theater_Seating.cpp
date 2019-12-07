0c356aed55d7134fdaf3ebc245390783eefa7786
Reece Reklai <reece.reklai@wallawalla.edu>
2019-12-07 00:38:25 +0000

    autosave

--- projects/proj03/Theater_Seating.cpp
+++ projects/proj03/Theater_Seating.cpp
@@ -7,27 +7,49 @@ Date: 12/6/19
 Project 3
 
  */
+#include <fstream>
 #include <iomanip>  // for format output
 #include <iostream> // for cout and cin
 #include <vector>   // for vectors
-#include <fstream> 
 using namespace std;
 
 // Functions and Global Variable
 ofstream fout;
 ifstream fin;
 // Functions
-char menu(char Choices) {
+int menuMain(int Choices) {
   cout << "Welcome to the reservation seat program" << endl;
   cout << "---------------------------------------" << endl;
-  cout << "(A) Display a seating chart" << endl;
-  cout << "(B) Sell one or more tickets" << endl;
-  cout << "(C) Display statistics such as the number of tickets sold, the "
+  cout << " 1) Display a seating chart" << endl;
+  cout << " 2) Sell one or more tickets" << endl;
+  cout << " 3) Display statistics such as the number of tickets sold, the "
           "number of seats still available, the total revenue from ticket sales"
        << endl;
-  cout << "(D) Reset the program and re-enter the theater seating and pricing "
+  cout << " 4) Reset the program and re-enter the theater seating and pricing "
           "information"
        << endl;
+  cout << " 5) Exit program" << endl;
+
+  if (Choices != 1 && Choices != 2 && Choices != 3 && Choices != 4 &&
+      Choices != 5) {
+    while (Choices != 1 && Choices != 2 && Choices != 3 && Choices != 4 &&
+           Choices != 5) {
+      cout << "Please make a decision" << endl;
+      cout << " 1) Display a seating chart" << endl;
+      cout << " 2) Sell one or more tickets" << endl;
+      cout << " 3) Display statistics such as the number of tickets sold, the "
+              "number of seats still available, the total revenue from ticket "
+              "sales"
+           << endl;
+      cout << " 4) Reset the program and re-enter the theater seating and "
+              "pricing "
+              "information"
+           << endl;
+      cout << " 5) Exit program" << endl;
+      cin >> Choices;
+    }
+    return Choices;
+  }
   return Choices;
 }
 void updateRowCol(int &row, int &column) {
@@ -57,35 +79,72 @@ void updateRowCol(int &row, int &column) {
   row = row;
   column = column;
 }
-void printToConfig(vector<double> &priceRows, int &row, int &column) {
-fout.open("theater.txt");
-if(!fout.is_open()){
+void readingToConfig(int &tickets, int &soldTickets, double &Revenue) {
+  fout.open("money.txt");
+  if (!fout.is_open()) {
     cout << "Could not be open" << endl;
-} else {
+  } else {
+    fout << tickets << endl;
+    fout << soldTickets << endl;
+    fout << Revenue << endl;
+  }
+  fout.close();
+}
+void readingFromConfig(int &tickets, int &soldTickets, double &Revenue) {
+  fin.open("money.txt");
+  if (!fin.is_open()) {
+    cout << "Could not be open" << endl;
+  } else {
+    fout << tickets << endl;
+    fout << soldTickets << endl;
+    fout << Revenue << endl;
+  }
+  fout.close();
+}
+void printToConfig(vector<double> &priceRows, int &row, int &column) {
+  fout.open("theater.txt");
+  if (!fout.is_open()) {
+    cout << "Could not be open" << endl;
+  } else {
     fout << row << endl;
     fout << column << endl;
-
-    for(int i =0; i < row; i++){
-        fout << priceRows.at(i) << endl;
+    for (int i = 0; i < row; i++) {
+      fout << priceRows.at(i) << endl;
     }
+  }
+  fout.close();
 }
-fout.close();
-}
-void readFromConfig(vector<double> &priceRows, int &row, int &column){
-    
+void readFromConfig(vector<double> &priceRows, int &row, int &column) {
+  double temp;
+  fin.open("theater.txt");
+
+  if (!fin.is_open()) {
+    cout << "Sorry file failed to open" << endl;
+  } else {
+    fin >> row;
+    fin >> column;
+    for (int i = 0; i < row; i++) {
+      fin >> temp;
+      priceRows.push_back(temp);
+    }
+    fin.close();
+  }
 }
 
 int main() {
+  int mainMenu;
   int row;
   int column;
+  int loopCount = 0;
+  int printCount = 0;
   int printRow = 0;
   int printCol = 1;
   int printR = 0;
   int printC = 1;
+  int ticketCount = 0;
   int tickets = 0;
-  double price;
-  double Revenue = 0;
-  double finalRevenue = 0;
+  int soldTickets = 0;
+  int money;
   int rowCount = 1;
   int rowNum;
   int colNum;
@@ -95,26 +154,50 @@ int main() {
   int numSeats = 0;
   int choiceSeat;
   int sorry = 0;
+  double price;
+  double Revenue = 0;
   char decisionReserve;
   char menu;
   bool successOrFail;
   bool decide = false;
   vector<double> priceRows(0);
   do {
-    cout << "Welcome to the reservation seat program" << endl;
-    cout << "---------------------------------------" << endl;
-    cout << "(A) Display a seating chart" << endl;
-    cout << "(B) Sell one or more tickets" << endl;
-    cout << "(C) Display statistics such as the number of tickets sold, the "
-            "number of seats still available, the total revenue from ticket "
-            "sales"
-         << endl;
-    cout
-        << "(D) Reset the program and re-enter the theater seating and pricing "
-           "information"
-        << endl;
+    if (printCount > 0) {
+      printR = 0;
+      printC = 1;
+    }
+    // menuMain(mainMenu);
+    // switch (menuMain(mainMenu)) {
     updateRowCol(row, column);
     tickets = row * column;
+    cout << endl;
+    cout << "What is the price for each row?" << endl;
+    do {
+      cout << "Row " << rowCount << ": $";
+      cin >> price;
+      if (price < 0) {
+        while (price < 0) {
+          cout << endl;
+          cout << "Please enter a valid integer" << endl;
+          cout << "what is the price for row " << rowCount << ": ";
+          cin >> price;
+          cout << endl;
+        }
+
+        priceRows.push_back(price);
+      } else {
+        priceRows.push_back(price);
+      }
+      rowCount++;
+    } while (priceRows.size() < row);
+    printToConfig(priceRows, row, column);
+    cout << endl;
+    cout << "Here are the prices for each row:" << endl;
+    for (int r = 0; r < priceRows.size(); r++) {
+      cout << "Row " << counter << ": $" << priceRows.at(r) << endl;
+      counter++;
+    }
+    cout << endl;
     char initializeSeat[row][column];
     cout << endl;
     cout << "Front Row Seats" << endl;
@@ -156,42 +239,10 @@ int main() {
     cout << endl;
     cout << "Back Row Seats" << endl;
     cout << endl;
-    cout
-        << "All seats are currently free in the theater shown above, there are "
-        << row;
-    cout << " rows and ";
-    cout << column << " columns" << endl;
-    cout << "The prices for each row of seats is up to your discretion" << endl;
-    cout << "Starting from the front row, what is the price for each row "
-         << endl;
-    do {
-      cout << "Row " << rowCount << ": $";
-      cin >> price;
-      if (price < 0) {
-        while (price < 0) {
-          cout << endl;
-          cout << "Please enter a valid integer" << endl;
-          cout << "what is the price for row " << rowCount << ": ";
-          cin >> price;
-          cout << endl;
-        }
-
-        priceRows.push_back(price);
-      } else {
-        priceRows.push_back(price);
-      }
-      rowCount++;
-    } while (priceRows.size() < row);
-    printToConfig(priceRows,row, column);
     cout << endl;
-    cout << "Here are the prices for each row:" << endl;
-    for (int r = 0; r < priceRows.size(); r++) {
-      cout << "Row " << counter << ": $" << priceRows.at(r) << endl;
-      counter++;
-    }
-    cout << endl;
+    printCount++;
     while (tickets > 0) {
-      cout << "Are there any seats you want to reserve? (Y)es or (N)o " << endl;
+      cout << "Do you want to reserve a seat? (Y)es or (N)o " << endl;
       cout << "Response: ";
       cin >> decisionReserve;
       if (decisionReserve != 'Y' && decisionReserve != 'y' &&
@@ -199,15 +250,21 @@ int main() {
         while (decisionReserve != 'Y' && decisionReserve != 'y' &&
                decisionReserve != 'N' && decisionReserve != 'n') {
           cout << "Please make a valid choice, do you want to reserve any "
-                  "seats? "
+                  "seat? "
                   "(Y)es or (N)o "
                << endl;
           cin >> decisionReserve;
         }
       } else if (decisionReserve == 'Y' || decisionReserve == 'y') {
-        if (tickets == 1) {
+        if (tickets == 1 || column == 1) {
+          cout << "Sorry only one seat is available, do you want to reserve "
+                  "one seat?"
+               << endl;
+          cout << " 1) = One seat" << endl;
+          cout << " 3) = Exit reservation" << endl;
+          cout << "Answer: ";
+          cin >> choiceSeat;
           while (choiceSeat != 1 && choiceSeat != 3) {
-
             cout << "Sorry only one seat is available, do you want to reserve "
                     "one seat?"
                  << endl;
@@ -216,21 +273,23 @@ int main() {
             cout << "Answer: ";
             cin >> choiceSeat;
           }
-        }
-        cout << "Do you want to reserve one seat or muliple seats?" << endl;
-        cout << " 1) = One seat" << endl;
-        cout << " 2) = Muliple seats" << endl;
-        cout << " 3) = Exit reservation" << endl;
-        cout << "Answer: ";
-        cin >> choiceSeat;
-        if (choiceSeat != 1 && choiceSeat != 2 && choiceSeat != 3) {
-          while (choiceSeat != 1 && choiceSeat != 2 && choiceSeat != 3) {
-            cout << "Please Enter 1 or 2 or 3" << endl;
-            cout << "Do you want to reserve one seat or muliple seats?" << endl;
-            cout << " 1) = One seat" << endl;
-            cout << " 2) = Muliple seats" << endl;
-            cout << " 3) = Exit reservation" << endl;
-            cin >> choiceSeat;
+        } else {
+          cout << "Do you want to reserve one seat or muliple seats?" << endl;
+          cout << " 1) = One seat" << endl;
+          cout << " 2) = Muliple seats" << endl;
+          cout << " 3) = Exit reservation" << endl;
+          cout << "Answer: ";
+          cin >> choiceSeat;
+          if (choiceSeat != 1 && choiceSeat != 2 && choiceSeat != 3) {
+            while (choiceSeat != 1 && choiceSeat != 2 && choiceSeat != 3) {
+              cout << "Please Enter 1 or 2 or 3" << endl;
+              cout << "Do you want to reserve one seat or muliple seats?"
+                   << endl;
+              cout << " 1) = One seat" << endl;
+              cout << " 2) = Muliple seats" << endl;
+              cout << " 3) = Exit reservation" << endl;
+              cin >> choiceSeat;
+            }
           }
         }
         switch (choiceSeat) {
@@ -266,8 +325,13 @@ int main() {
           if (initializeSeat[rowNum][colNum] != '*') {
             initializeSeat[rowNum][colNum] = '*';
           }
+          if (loopCount > 0) {
+            printRow = 0;
+            printCol = 1;
+          }
           cout << "Front Row Seats" << endl;
           cout << endl;
+          cout << "Tens    ";
           for (int t = 1; t <= column; t++) {
             if (t == 1) {
               cout << "0";
@@ -277,6 +341,7 @@ int main() {
               cout << " ";
             }
           }
+          cout << endl;
           cout << endl;
           cout << "Column  ";
           for (int c = 1; c <= column; c++) {
@@ -298,29 +363,34 @@ int main() {
             }
             for (int c = 1; c <= column; c++) {
               if (initializeSeat[r][c] == '*') {
-                cout << '*';
-                Revenue += priceRows[r];
-                tickets = tickets - 1;
+                cout << initializeSeat[r][c];
+                ticketCount++;
+                money = priceRows[r] + money;
                 successOrFail = true;
               } else {
-                cout << '#';
+                cout << initializeSeat[r][c];
               }
             }
             cout << endl;
           }
+          loopCount++;
           if (successOrFail != true) {
             successOrFail = false;
           } else {
             successOrFail = true;
+            Revenue = money;
+            soldTickets = ticketCount;
+            tickets = tickets - soldTickets;
+            readingToConfig(tickets, soldTickets, Revenue);
           }
           cout << endl;
           cout << "Back Row Seats" << endl;
           if (successOrFail == true) {
             cout << endl;
-            cout << "You have successfully secure a seat";
+            cout << "You have successfully secure a seat" << endl;
           } else {
             cout << endl;
-            cout << "Sorry, the seat has been taken.";
+            cout << "Sorry, the seat has been taken." << endl;
           }
           break;
         case 2:
@@ -405,8 +475,13 @@ int main() {
               }
             }
           }
+          if (loopCount > 0) {
+            printRow = 0;
+            printCol = 1;
+          }
           cout << "Front Row Seats" << endl;
           cout << endl;
+          cout << "Tens    ";
           for (int t = 1; t <= column; t++) {
             if (t == 1) {
               cout << "0";
@@ -416,6 +491,7 @@ int main() {
               cout << " ";
             }
           }
+          cout << endl;
           cout << endl;
           cout << "Column  ";
           for (int c = 1; c <= column; c++) {
@@ -438,8 +514,8 @@ int main() {
             for (int c = 1; c <= column; c++) {
               if (initializeSeat[r][c] == '*') {
                 cout << '*';
-                Revenue += priceRows[r];
-                tickets = tickets - 1;
+                ticketCount++;
+                money = priceRows[r] + money;
                 successOrFail = true;
               } else {
                 cout << '#';
@@ -447,19 +523,24 @@ int main() {
             }
             cout << endl;
           }
+          loopCount++;
           if (successOrFail != true) {
             successOrFail = false;
           } else {
             successOrFail = true;
+            Revenue = money;
+            soldTickets = ticketCount;
+            tickets = tickets - soldTickets;
+            readingToConfig(tickets, soldTickets, Revenue);
           }
           cout << endl;
           cout << "Back Row Seats" << endl;
           if (successOrFail == true) {
             cout << endl;
-            cout << "You have successfully secure a seat";
+            cout << "You have successfully secure a seat" << endl;
           } else {
             cout << endl;
-            cout << "Sorry, the seat has been taken.";
+            cout << "Sorry, the seat has been taken." << endl;
           }
           break;
         case 3:
@@ -477,16 +558,16 @@ int main() {
     cout << "Do you want to see menu? (Y)es or (N)o " << endl;
     cin >> menu;
     if (menu != 'Y' && menu != 'y' && menu != 'N' && menu != 'n') {
-      cout
-          << "Please give valid answer, do you want to see menu? (Y)es or (N)o "
-          << endl;
+      cout << "Please give valid answer, do you want to see menu? (Y)es or "
+              "(N)o "
+           << endl;
       cin >> menu;
     } else if (menu == 'Y' || menu == 'y') {
       menu = 'Y';
     } else if (menu == 'N' || menu == 'n') {
       exit(0);
     }
-    cout << endl;
+    //}
   } while (menu == 'Y' || menu == 'y');
   return 0;
 }
