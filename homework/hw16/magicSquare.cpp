0d0c807a7cb117e24ce4d54a1cc0a69fa605b286
Reece Reklai <reece.reklai@wallawalla.edu>
2019-12-10 23:31:40 +0000

    autosave

--- homework/hw16/magicSquare.cpp
+++ homework/hw16/magicSquare.cpp
@@ -41,86 +41,90 @@ bool isLoShu(int array[][SIZE], int rows) {
   if (shape.size() != shaping.size()) {
     loShuMagicSquare = false;
     return loShuMagicSquare;
-  } else if (shape.size() == shaping.size()) {
-    for (int r = 1; r <= SIZE; r++) {
-      for (int c = 1; c <= SIZE; c++) {
-        if (array[r][c] == array[1][1] || array[r][c] == array[1][2] ||
-            array[r][c] == array[1][3] || array[r][c] == array[2][1] ||
-            array[r][c] == array[2][2] || array[r][c] == array[2][3] ||
-            array[r][c] == array[3][1] || array[r][c] == array[3][2] ||
-            array[r][c] == array[3][3]) {
-          counter++;
-        }
-        if (r == 1) {
-          storageRow += array[r][c];
-        }
-        if (c == 1) {
-          storeCol += array[r][c];
-        }
-        if (r == 2) {
-          storageRow2 += array[r][c];
-        }
-        if (c == 2) {
-          storeCol2 += array[r][c];
-        }
-        if (r == 3) {
-          storageRow3 += array[r][c];
-        }
-        if (c == 3) {
-          storeCol3 += array[r][c];
-        }
-        if (r == 1 && c == 1) {
-          r1c1 = array[r][c];
-        }
-        if (r == 2 && c == 2) {
-          r2c2 = array[r][c];
-        }
-        if (r == 3 && c == 3) {
-          r3c3 = array[r][c];
-        }
-        if (r == 3 && c == 1) {
-          r3c1 = array[r][c];
-        }
-        if (r == 1 && c == 3) {
-          r1c3 = array[r][c];
-        }
-        if (r == 1 && c == 2) {
-          r1c2 = array[r][c];
-        }
-        if (r == 2 && c == 1) {
-          r2c1 = array[r][c];
-        }
-        if (r == 2 && c == 3) {
-          r2c3 = array[r][c];
-        }
-        if (r == 3 && c == 2) {
-          r3c2 = array[r][c];
+  }
+  if (shape.size() == shaping.size()) {
+    loShuMagicSquare = true;
+    return loShuMagicSquare;
+  }
+
+  for (int r = 1; r <= SIZE; r++) {
+    for (int c = 1; c <= SIZE; c++) {
+      if (array[r][c] == array[1][1] || array[r][c] == array[1][2] ||
+          array[r][c] == array[1][3] || array[r][c] == array[2][1] ||
+          array[r][c] == array[2][2] || array[r][c] == array[2][3] ||
+          array[r][c] == array[3][1] || array[r][c] == array[3][2] ||
+          array[r][c] == array[3][3]) {
+        counter++;
+      }
+      if (r == 1) {
+        storageRow += array[r][c];
+      }
+      if (c == 1) {
+        storeCol += array[r][c];
+      }
+      if (r == 2) {
+        storageRow2 += array[r][c];
+      }
+      if (c == 2) {
+        storeCol2 += array[r][c];
+      }
+      if (r == 3) {
+        storageRow3 += array[r][c];
+      }
+      if (c == 3) {
+        storeCol3 += array[r][c];
+      }
+      if (r == 1 && c == 1) {
+        r1c1 = array[r][c];
+      }
+      if (r == 2 && c == 2) {
+        r2c2 = array[r][c];
+      }
+      if (r == 3 && c == 3) {
+        r3c3 = array[r][c];
+      }
+      if (r == 3 && c == 1) {
+        r3c1 = array[r][c];
+      }
+      if (r == 1 && c == 3) {
+        r1c3 = array[r][c];
+      }
+      if (r == 1 && c == 2) {
+        r1c2 = array[r][c];
+      }
+      if (r == 2 && c == 1) {
+        r2c1 = array[r][c];
+      }
+      if (r == 2 && c == 3) {
+        r2c3 = array[r][c];
+      }
+      if (r == 3 && c == 2) {
+        r3c2 = array[r][c];
+      }
+    }
+  }
+  store = r1c1 + r2c2 + r3c3;
+  store2 = r3c1 + r2c2 + r1c3;
+  if (counter == 9) {
+    loShuMagicSquare = true;
+  } else {
+    loShuMagicSquare = false;
+  }
+  if (storageRow == storageRow2 && storageRow == storageRow3 &&
+      storageRow2 == storageRow3) {
+    if (storeCol == storeCol2 && storeCol == storeCol3 &&
+        storeCol2 == storeCol3) {
+      if (store == store2) {
+        if (storageRow == storeCol && storageRow == store &&
+            storeCol == store) {
+          loShuMagicSquare = true;
+          return loShuMagicSquare;
         }
       }
     }
-    store = r1c1 + r2c2 + r3c3;
-    store2 = r3c1 + r2c2 + r1c3;
-    if (counter == 9) {
-      loShuMagicSquare = true;
-    } else {
-      loShuMagicSquare = false;
-    }
-    if (storageRow == storageRow2 && storageRow == storageRow3 &&
-        storageRow2 == storageRow3) {
-      if (storeCol == storeCol2 && storeCol == storeCol3 &&
-          storeCol2 == storeCol3) {
-        if (store == store2) {
-          if (storageRow == storeCol && storageRow == store &&
-              storeCol == store) {
-            loShuMagicSquare = true;
-            return loShuMagicSquare;
-          }
-        }
-      }
-    } else {
-      loShuMagicSquare = false;
-      return loShuMagicSquare;
-    }
+  } else {
+    loShuMagicSquare = false;
+    return loShuMagicSquare;
   }
   return loShuMagicSquare;
 }
