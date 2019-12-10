d20e1a569bf5cb8226623f0a99d7ac372933d5b6
Reece Reklai <reece.reklai@wallawalla.edu>
2019-12-10 23:17:40 +0000

    autosave

--- homework/hw16/magicSquare.cpp
+++ homework/hw16/magicSquare.cpp
@@ -34,67 +34,76 @@ bool isLoShu(int array[][SIZE], int rows) {
   int store = 0;
   int store2 = 0;
   int counter = 0;
-  int comparisonArray[SIZE];
-  bool valid;
-  for (int r = 1; r <= SIZE; r++) {
-    for (int c = 1; c <= SIZE; c++) {
-      if (array[r][c] == array[1][1] || array[r][c] == array[1][2] ||
-          array[r][c] == array[1][3] || array[r][c] == array[2][1] ||
-          array[r][c] == array[2][2] || array[r][c] == array[2][3] ||
-          array[r][c] == array[3][1] || array[r][c] == array[3][2] ||
-          array[r][c] == array[3][3]) {
-        counter++;
-      }
-      if (r == 1) {
-        storageRow += array[r][c];
-      }
-      if (c == 1) {
-        storeCol += array[r][c];
-      }
-      if (r == 2) {
-        storageRow2 += array[r][c];
-      }
-      if (c == 2) {
-        storeCol2 += array[r][c];
-      }
-      if (r == 3) {
-        storageRow3 += array[r][c];
-      }
-      if (c == 3) {
-        storeCol3 += array[r][c];
-      }
-      if (r == 1 && c == 1) {
-        r1c1 = array[r][c];
-      }
-      if (r == 2 && c == 2) {
-        r2c2 = array[r][c];
-      }
-      if (r == 3 && c == 3) {
-        r3c3 = array[r][c];
-      }
-      if (r == 3 && c == 1) {
-        r3c1 = array[r][c];
-      }
-      if (r == 1 && c == 3) {
-        r1c3 = array[r][c];
-      }
-      if (r == 1 && c == 2) {
-        r1c2 = array[r][c];
-      }
-      if (r == 2 && c == 1) {
-        r2c1 = array[r][c];
-      }
-      if (r == 2 && c == 3) {
-        r2c3 = array[r][c];
-      }
-      if (r == 3 && c == 2) {
-        r3c2 = array[r][c];
+  int comparisonArray[rows][SIZE];
+  bool loShuMagicSquare;
+  if (array != comparisonArray) {
+    loShuMagicSquare = false;
+    return loShuMagicSquare;
+  }
+  if (array == comparisonArray) {
+    for (int r = 1; r <= SIZE; r++) {
+      for (int c = 1; c <= SIZE; c++) {
+        if (array[r][c] == array[1][1] || array[r][c] == array[1][2] ||
+            array[r][c] == array[1][3] || array[r][c] == array[2][1] ||
+            array[r][c] == array[2][2] || array[r][c] == array[2][3] ||
+            array[r][c] == array[3][1] || array[r][c] == array[3][2] ||
+            array[r][c] == array[3][3]) {
+          counter++;
+        }
+        if (r == 1) {
+          storageRow += array[r][c];
+        }
+        if (c == 1) {
+          storeCol += array[r][c];
+        }
+        if (r == 2) {
+          storageRow2 += array[r][c];
+        }
+        if (c == 2) {
+          storeCol2 += array[r][c];
+        }
+        if (r == 3) {
+          storageRow3 += array[r][c];
+        }
+        if (c == 3) {
+          storeCol3 += array[r][c];
+        }
+        if (r == 1 && c == 1) {
+          r1c1 = array[r][c];
+        }
+        if (r == 2 && c == 2) {
+          r2c2 = array[r][c];
+        }
+        if (r == 3 && c == 3) {
+          r3c3 = array[r][c];
+        }
+        if (r == 3 && c == 1) {
+          r3c1 = array[r][c];
+        }
+        if (r == 1 && c == 3) {
+          r1c3 = array[r][c];
+        }
+        if (r == 1 && c == 2) {
+          r1c2 = array[r][c];
+        }
+        if (r == 2 && c == 1) {
+          r2c1 = array[r][c];
+        }
+        if (r == 2 && c == 3) {
+          r2c3 = array[r][c];
+        }
+        if (r == 3 && c == 2) {
+          r3c2 = array[r][c];
+        }
       }
     }
-  }
-  store = r1c1 + r2c2 + r3c3;
-  store2 = r3c1 + r2c2 + r1c3;
-  if (counter == 9) {
+    store = r1c1 + r2c2 + r3c3;
+    store2 = r3c1 + r2c2 + r1c3;
+    if (counter == 9) {
+      loShuMagicSquare = true;
+    } else {
+      loShuMagicSquare = false;
+    }
     if (storageRow == storageRow2 && storageRow == storageRow3 &&
         storageRow2 == storageRow3) {
       if (storeCol == storeCol2 && storeCol == storeCol3 &&
@@ -102,12 +111,13 @@ bool isLoShu(int array[][SIZE], int rows) {
         if (store == store2) {
           if (storageRow == storeCol && storageRow == store &&
               storeCol == store) {
-            return valid = true;
+            loShuMagicSquare = true;
           }
         }
       }
+    } else {
+      loShuMagicSquare = false;
     }
-  } else {
-    return valid = false;
   }
+  return loShuMagicSquare;
 }
