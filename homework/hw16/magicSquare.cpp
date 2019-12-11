5bad5309eb7b60aacb531ede5a0e1f419edbecbd
Reece Reklai <reece.reklai@wallawalla.edu>
2019-12-11 00:11:01 +0000

    autosave

--- homework/hw16/magicSquare.cpp
+++ homework/hw16/magicSquare.cpp
@@ -16,8 +16,15 @@ using namespace std;
 
 bool isLoShu(int array[][SIZE], int rows) {
   assert(rows == 3);
-  int sum = 0;
-  int sum2 = 0;
+  int sumRow1 = 0;
+  int sumRow2 = 0;
+  int sumRow3 = 0;
+  int sumCol1 = 0;
+  int sumCol2 = 0;
+  int sumCol3 = 0;
+  int sumCross1 = 0;
+  int sumCross2 = 0;
+  int counter = 0;
   bool loShuMagicSquare;
   vector<int> shape(SIZE);
   vector<int> shaping(rows);
@@ -26,8 +33,33 @@ bool isLoShu(int array[][SIZE], int rows) {
   } else if (shape.size() == shaping.size()) {
     for (int r = 0; r < 3; r++) {
       for (int c = 0; c < 3; c++) {
-         return true;
+        sumRow1 += array[0][c];
+        sumRow2 += array[1][c];
+        sumRow3 += array[2][c];
+        sumCol1 += array[r][0];
+        sumCol2 += array[r][1];
+        sumCol3 += array[r][2];
+        sumCross1 = array[0][0] + array[1][1] + array[2][2];
+        sumCross2 = array[2][0] + array[1][1] + array[0][2];
+        if (array[r][c] == array[0][0] || array[r][c] == array[0][1] ||
+            array[r][c] == array[0][2] || array[r][c] == array[1][0] ||
+            array[r][c] == array[1][1] || array[r][c] == array[1][2] ||
+            array[r][c] == array[2][0] || array[r][c] == array[2][1] ||
+            array[r][c] == array[2][2]) {
+          counter++;
+        }
       }
     }
+    if (counter == 9) {
+      if (sumRow1 == sumRow2 && sumRow2 == sumRow3 && sumRow3 == sumCol1 &&
+          sumCol1 == sumCol2 && sumCol2 == sumCol3 && sumCol3 == sumCross1 &&
+          sumCross1 == sumCross2) {
+        return true;
+      }
+    } else {
+      return false;
+    }
+    return false;
   }
+  return false;
 }
